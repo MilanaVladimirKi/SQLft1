8f0164376d517ab983f240a0302580b224834467
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-10-07 21:05:54 +0500

    Page

---
+++ src/test/java/ru/topacademy/page/VerificationPage.java
@@ -0,0 +1,27 @@
+package ru.topacademy.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+    private SelenideElement codeField = $("[data-test-id=code] input");
+    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    private SelenideElement errorNotification = $("[data-test-id=error-notification]");
+
+    public void verifyVerificationPageVisible() { codeField.shouldBe(visible); }
+    public void errorNotificationVisible() { errorNotification.shouldBe(visible); }
+
+    public DashboardPage validVerify(String verificationCode) {
+        verifyButtonClick(verificationCode);
+        return new DashboardPage();
+    }
+
+    public void verifyErrorNotificationVisible() {
+        errorNotification.shouldBe(visible, Duration.ofSeconds(10));
+    }
+}
